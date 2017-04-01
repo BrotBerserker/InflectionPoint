7357df5c45dd2acfb8ee98ef800aa8978c736d98
Readock <master-funghu@gmx.de>
2017-04-01 21:47:04 +0200

    adds MainMenu classes

---
+++ InflectionPoint/Source/InflectionPoint/UI/Menus/MainMenuBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "MainMenuBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFLECTIONPOINT_API UMainMenuBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
