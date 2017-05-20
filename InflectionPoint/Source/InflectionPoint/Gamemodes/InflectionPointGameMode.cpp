fa024578df73c5a05040105cade0451126ec427a
Sigi <shaiyaaccount2@web.de>
2017-05-20 19:23:44 +0200

    Adds and uses instant respawn functionality

--- InflectionPoint/Source/InflectionPoint/Gamemodes/InflectionPointGameMode.cpp
+++ InflectionPoint/Source/InflectionPoint/Gamemodes/InflectionPointGameMode.cpp
@@ -4,13 +4,29 @@
 #include "InflectionPointGameMode.h"
 #include "UI/HUD/InflectionPointHUD.h"
 #include "Gameplay/Characters/BaseCharacter.h"
+#include "Gameplay/Characters/PlayerControlledFPSCharacter.h"
 
 AInflectionPointGameMode::AInflectionPointGameMode()
 	: Super() {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/InflectionPoint/Blueprints/Characters/PlayerCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
-
+	
 	// use our custom HUD class
 	HUDClass = AInflectionPointHUD::StaticClass();
+}
+
+void AInflectionPointGameMode::PlayerDied(APlayerController * playerController) {
+	AActor* playerStart = GetWorld()->GetAuthGameMode()->ChoosePlayerStart(playerController);
+	AssertNotNull(playerStart, GetWorld(), __FILE__, __LINE__);
+
+	FVector loc = playerStart->GetTransform().GetLocation();
+	FRotator rot = FRotator(playerStart->GetTransform().GetRotation());
+
+	FActorSpawnParameters ActorSpawnParams;
+	ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	APlayerControlledFPSCharacter* newCharacter = GetWorld()->SpawnActor<APlayerControlledFPSCharacter>(DefaultPawnClass.Get(), loc, rot, ActorSpawnParams);
+	
+	playerController->Possess(newCharacter);
 }
