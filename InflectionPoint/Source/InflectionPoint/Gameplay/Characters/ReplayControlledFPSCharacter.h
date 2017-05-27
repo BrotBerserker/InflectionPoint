00eadfd04c4461058dea1d733fd55a160f3b8c79
Readock <readock@gmx.de>
2017-05-27 16:20:41 +0200

    adds replay implementation based on TMap<FString, TArray<TTuple<float, float, float>>>

--- InflectionPoint/Source/InflectionPoint/Gameplay/Characters/ReplayControlledFPSCharacter.h
+++ InflectionPoint/Source/InflectionPoint/Gameplay/Characters/ReplayControlledFPSCharacter.h
@@ -14,14 +14,21 @@ class INFLECTIONPOINT_API AReplayControlledFPSCharacter : public ABaseCharacter 
 	GENERATED_BODY()
 
 public:
+	// Sets default values for this component's properties
+	AReplayControlledFPSCharacter();
 
 	virtual void BeginPlay() override;
 
-	void StartReplay(TArray<TPair<FKey, TTuple<float, float, float>>> inputs, TArray<float> moveForwards, TArray<float> moveRights);
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void StartReplay(TMap<FString, TArray<TTuple<float, float, float>>> keysPressed, TMap<FString, TArray<TTuple<float, float, float>>> keysReleased);
 
 
 	UFUNCTION()
-		void PressKey(float yaw, float pitch, FKey key);
+		void PressKey(float yaw, float pitch, FString key);
+	UFUNCTION()
+		void ReleaseKey(float yaw, float pitch, FString key);
 
 	UFUNCTION()
 		void ApplyYaw(float value);
@@ -35,4 +42,14 @@ public:
 	UFUNCTION()
 		void ReplayMoveRight(float value);
 
+
+	TMap<FKey, TArray<TTuple<float, float, float>>> KeysPressed;
+	TMap<FKey, TArray<TTuple<float, float, float>>> KeysReleased;
+
+	bool isForwardPressed = false;
+	bool isBackwordPressed = false;
+	bool isRightPressed = false;
+	bool isLeftPressed = false;
+private:
+	void StartTimerForKeyChanged(TPair<FString, TArray<TTuple<float, float, float>>> & element, FString timerFunction);
 };
