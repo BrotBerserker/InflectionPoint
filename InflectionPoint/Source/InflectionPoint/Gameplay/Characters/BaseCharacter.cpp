57394b14c60f7708495a0707b4b65e74ddd64227
Sigi <shaiyaaccount2@web.de>
2017-06-10 14:36:01 +0200

    Adds animations for aiming up and down

--- InflectionPoint/Source/InflectionPoint/Gameplay/Characters/BaseCharacter.cpp
+++ InflectionPoint/Source/InflectionPoint/Gameplay/Characters/BaseCharacter.cpp
@@ -23,7 +23,8 @@ ABaseCharacter::ABaseCharacter() {
 	// Create a CameraComponent	
 	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
 	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
-	FirstPersonCameraComponent->RelativeLocation = FVector(-39.56f, 1.75f, 64.f); // Position the camera
+	//FirstPersonCameraComponent->RelativeLocation = FVector(-39.56f, 1.75f, 64.f); 
+	FirstPersonCameraComponent->RelativeLocation = FVector(-20.5f, 1.75f, 41.f); // Position the camera
 	FirstPersonCameraComponent->bUsePawnControlRotation = true;
 
 	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
