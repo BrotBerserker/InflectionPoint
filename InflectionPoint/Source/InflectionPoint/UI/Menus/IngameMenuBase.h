b4cf8ecb6e224ec4b682e371b79fa172f2a5f161
Sigi <shaiyaaccount2@web.de>
2017-06-17 16:12:11 +0200

    Adds IngameMenu and binds it to "Escape"

---
+++ InflectionPoint/Source/InflectionPoint/UI/Menus/IngameMenuBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "IngameMenuBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFLECTIONPOINT_API UIngameMenuBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
