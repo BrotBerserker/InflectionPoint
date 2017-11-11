a8ce976572a8b2f018576f849cae424d9193747d
Readock <readock@gmx.de>
2017-11-11 14:45:11 +0100

    Adds PlayerState attributes
    - Adds: Deaths, PlayerKills, ReplayKills
    - Scoreboard uses PlayerState attributes

--- InflectionPoint/Source/InflectionPoint/Gamemodes/TDMPlayerStateBase.cpp
+++ InflectionPoint/Source/InflectionPoint/Gamemodes/TDMPlayerStateBase.cpp
@@ -10,4 +10,35 @@ void ATDMPlayerStateBase::GetLifetimeReplicatedProps(TArray< FLifetimeProperty >
 	DOREPLIFETIME(ATDMPlayerStateBase, Team);
 	DOREPLIFETIME(ATDMPlayerStateBase, PlayerStartGroup);
 	DOREPLIFETIME(ATDMPlayerStateBase, PlayerName);
+	DOREPLIFETIME(ATDMPlayerStateBase, PlayerKills);
+	DOREPLIFETIME(ATDMPlayerStateBase, ReplayKills);
+	DOREPLIFETIME(ATDMPlayerStateBase, Deaths);
+}
+
+void ATDMPlayerStateBase::AddPlayerKill() {
+	if(!HasAuthority())
+		return;
+	Score += 100;
+	PlayerKills++;
+}
+void ATDMPlayerStateBase::AddReplayKill() {
+	if(!HasAuthority())
+		return;
+	Score += 50;
+	ReplayKills++;
+}
+void ATDMPlayerStateBase::AddDeath() {
+	if(!HasAuthority())
+		return;
+	Deaths++;
+}
+
+int ATDMPlayerStateBase::GetPlayerKills() {
+	return PlayerKills;
+}
+int ATDMPlayerStateBase::GetReplayKills() {
+	return ReplayKills;
+}
+int ATDMPlayerStateBase::GetDeaths() {
+	return Deaths;
 }
--- InflectionPoint/Source/InflectionPoint/Gamemodes/TDMPlayerStateBase.h
+++ InflectionPoint/Source/InflectionPoint/Gamemodes/TDMPlayerStateBase.h
@@ -17,9 +17,34 @@ public:
 	void GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const;
 
 public:
+
 	UPROPERTY(Replicated, BlueprintReadWrite, Category = "InflectionPoint|Gameplay")
 		int Team = 0;
 
 	UPROPERTY(Replicated, BlueprintReadWrite, Category = "InflectionPoint|Gameplay")
 		FString PlayerStartGroup;
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "InflectionPoint|Gameplay")
+		void AddPlayerKill();
+	UFUNCTION(BlueprintCallable, Category = "InflectionPoint|Gameplay")
+		void AddReplayKill();
+	UFUNCTION(BlueprintCallable, Category = "InflectionPoint|Gameplay")
+		void AddDeath();
+
+	UFUNCTION(BlueprintCallable, Category = "InflectionPoint|Gameplay")
+		int GetPlayerKills();
+	UFUNCTION(BlueprintCallable, Category = "InflectionPoint|Gameplay")
+		int GetReplayKills();
+	UFUNCTION(BlueprintCallable, Category = "InflectionPoint|Gameplay")
+		int GetDeaths();
+
+private:
+	UPROPERTY(Replicated)
+		int PlayerKills = 0;
+	UPROPERTY(Replicated)
+		int ReplayKills = 0;
+	UPROPERTY(Replicated)
+		int Deaths = 0;
+
 };
