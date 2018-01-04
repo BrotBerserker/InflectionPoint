3ada2edb5768c78539646bdbd2c5dccd6d9e0822
readock <readock@gmx.de>
2018-01-04 18:19:16 +0100

    Updates Clear.bat and ClearUnrealOnly.bat
    - Clear will now clear all files and retry when failed
    (so you dont nede to call the *.bat more then once if unreal is still holding some files)

--- InflectionPoint/ClearAll.bat
+++ InflectionPoint/ClearAll.bat
@@ -1,7 +1,15 @@
+:repeat
 del *.VC.db
+if EXIST *.VC.db goto repeat
 del *.VC.VC.db
+if EXIST *.VC.VC.db goto repeat
 del *.sln
+if EXIST *.sln goto repeat
 rmdir Intermediate /S /Q 
+if EXIST Intermediate goto repeat
 rmdir Saved /S /Q
+if EXIST Saved goto repeat
 rmdir Binaries /S /Q
+if EXIST Binaries goto repeat
 rmdir .vs /S /Q
+if EXIST .vs goto repeat
--- InflectionPoint/ClearUnrealOnly.bat
+++ InflectionPoint/ClearUnrealOnly.bat
@@ -1,3 +1,7 @@
+:repeat
 rmdir Intermediate /S /Q 
+if EXIST Intermediate goto repeat
 rmdir Saved /S /Q
+if EXIST Saved goto repeat
 rmdir Binaries /S /Q
+if EXIST Binaries goto repeat
