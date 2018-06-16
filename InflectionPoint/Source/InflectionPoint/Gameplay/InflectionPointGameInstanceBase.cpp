79c4aa4f7e16f096b9acf329620ba990069132e7
readock <readock@gmx.de>
2018-06-16 17:44:26 +0200

    Implements loading screen

--- InflectionPoint/Source/InflectionPoint/Gameplay/InflectionPointGameInstanceBase.cpp
+++ InflectionPoint/Source/InflectionPoint/Gameplay/InflectionPointGameInstanceBase.cpp
@@ -8,3 +8,28 @@ UInflectionPointGameInstanceBase::UInflectionPointGameInstanceBase() {
 	IPSettings = CreateDefaultSubobject<UInflectionPointSettings>(TEXT("IPSettings"));
 }
 
+void UInflectionPointGameInstanceBase::Init() {
+	Super::Init();
+
+	FCoreUObjectDelegates::PreLoadMap.AddUObject(this, &UInflectionPointGameInstanceBase::BeginLoadingScreen);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &UInflectionPointGameInstanceBase::EndLoadingScreen);
+}
+
+void UInflectionPointGameInstanceBase::BeginLoadingScreen(const FString& InMapName) {
+	UE_LOG(LogTemp, Warning, TEXT("BeginLoadingScreen"));
+
+	if(!IsRunningDedicatedServer()) {
+		FLoadingScreenAttributes LoadingScreen;
+		LoadingScreen.bAutoCompleteWhenLoadingCompletes = false;
+		auto loadingWidget = CreateWidget<UUserWidget>(this, WidgetLoadingScreen);
+		
+		if(SoftAssertTrue(loadingWidget != nullptr, GetWorld(), __FILE__, __LINE__,"No Loading Screen Widget"))
+			LoadingScreen.WidgetLoadingScreen = loadingWidget->TakeWidget();
+
+		GetMoviePlayer()->SetupLoadingScreen(LoadingScreen);
+	}
+}
+
+void UInflectionPointGameInstanceBase::EndLoadingScreen(UWorld* InLoadedWorld) {
+	UE_LOG(LogTemp, Warning, TEXT("EndLoadingScreen"));
+}
--- InflectionPoint/Source/InflectionPoint/Gameplay/InflectionPointGameInstanceBase.h
+++ InflectionPoint/Source/InflectionPoint/Gameplay/InflectionPointGameInstanceBase.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Config/InflectionPointSettings.h"
 #include "Engine/GameInstance.h"
+#include "Runtime/MoviePlayer/Public/MoviePlayer.h"
 #include "InflectionPointGameInstanceBase.generated.h"
 
 /**
@@ -16,10 +18,20 @@ class INFLECTIONPOINT_API UInflectionPointGameInstanceBase : public UGameInstanc
 public:
 	UInflectionPointGameInstanceBase();
 
+	virtual void Init() override;
+
+	UFUNCTION()
+		virtual void BeginLoadingScreen(const FString& MapName);
+	UFUNCTION()
+		virtual void EndLoadingScreen(UWorld* InLoadedWorld);
 public:
 	UPROPERTY(BlueprintReadWrite)
 		FName CurrentSessionName;
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 		class UInflectionPointSettings* IPSettings;
+
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		TSubclassOf<class UUserWidget> WidgetLoadingScreen;
 };
