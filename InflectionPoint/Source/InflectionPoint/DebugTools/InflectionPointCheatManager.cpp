b6bc937116d58a6c81907761b669f0e8f029e717
readock <readock@gmx.de>
2018-11-26 21:25:11 +0100

    Implements Shop cheat

--- InflectionPoint/Source/InflectionPoint/DebugTools/InflectionPointCheatManager.cpp
+++ InflectionPoint/Source/InflectionPoint/DebugTools/InflectionPointCheatManager.cpp
@@ -2,6 +2,9 @@
 
 #include "InflectionPoint.h"
 #include "Gameplay/Damage/MortalityProvider.h"
+#include "Gameplay/Controllers/PlayerControllerBase.h"
+#include "Gameplay/Characters/BaseCharacter.h"
+#include "Gamemodes/TDMPlayerStateBase.h"
 #include "InflectionPointCheatManager.h"
 
 
@@ -29,6 +32,28 @@ void UInflectionPointCheatManager::EnableAllReplayDebugging(bool enable) {
 
 void UInflectionPointCheatManager::EnableHUD(bool enable) {
 	IsHUDEnabled = enable;
+}
+
+
+void UInflectionPointCheatManager::OpenShop() {
+	auto controller = Cast<APlayerControllerBase>(GetWorld()->GetFirstPlayerController());
+	auto playerState = Cast<ATDMPlayerStateBase>(controller->PlayerState);
+	playerState->IPPoints = 99;
+	controller->ClientShowShop(false);
+}
+
+void UInflectionPointCheatManager::CloseShop() {
+	auto controller = Cast<APlayerControllerBase>(GetWorld()->GetFirstPlayerController());
+	auto playerState = Cast<ATDMPlayerStateBase>(controller->PlayerState);
+	auto character = Cast<ABaseCharacter>(controller->GetPawn());
+	controller->ClientShowPhaseCountdownNumber(0); // to switch to ingame View
+	// Equipp Items
+	for(int i = 0; i < playerState->EquippedShopItems.Num(); i++) {
+		auto item = playerState->EquippedShopItems[i];
+		item.ShopItemClass.GetDefaultObject()->ApplyToCharacter(character, item.Slot);
+		if(i == 0)
+			character->ServerEquipSpecificWeapon(item.Slot);
+	}
 }
 
 //void UInflectionPointCheatManager::SetHP(int newHP) {
