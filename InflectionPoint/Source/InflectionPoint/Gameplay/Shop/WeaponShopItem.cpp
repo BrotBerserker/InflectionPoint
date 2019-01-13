0d7422db70b1a92cc6d61a6f5fc7836629cfb32b
readock <readock@gmx.de>
2019-01-13 01:34:43 +0100

    Adds nullcheck to weaponShopItem

--- InflectionPoint/Source/InflectionPoint/Gameplay/Shop/WeaponShopItem.cpp
+++ InflectionPoint/Source/InflectionPoint/Gameplay/Shop/WeaponShopItem.cpp
@@ -10,5 +10,7 @@ UWeaponShopItem::UWeaponShopItem() {
 }
 
 void UWeaponShopItem::ApplyToCharacter(ABaseCharacter* character, EInventorySlotPosition slot) {
+	AssertNotNull(character, GetWorld(), __FILE__, __LINE__);
+	AssertNotNull(character->WeaponInventory, GetWorld(), __FILE__, __LINE__);
 	character->WeaponInventory->SetWeaponAtPosition(slot, WeaponClass);
 }
